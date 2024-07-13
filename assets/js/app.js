d162542d82a3e2e7145bdf8522a731f09bd6c166
Hamid <hamidachaou379@gmail.com>
2024-07-13 01:59:37 +0100

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -80,3 +80,38 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 50)
 })
+
+// ========== togel icon navbar ===================================
+let menuIcon = document.querySelector("#menu-icon");
+let navList = document.querySelector(".navlist");
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle("bx-x");
+    navList.classList.toggle("open");
+}
+
+
+window.onscroll = () =>{
+    menuIcon.classList.remove("bx-x");
+    navList.classList.remove("open");
+}
+
+// ========== paralox ===================================
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry)=> {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("show-items")
+        } else {
+            entry.target.classList.remove("show-items")
+        }
+    })
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((ele)=> observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((ele)=> observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((ele)=> observer.observe(el));
